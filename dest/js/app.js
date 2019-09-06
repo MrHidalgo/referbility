9f3a77c3bb573ee1a808f56135eeadfa3295cb01
MrHidalgo <Luckyhidalgo@gmail.com>
2019-09-06 17:25:19 +0300

    Initial commit

---
+++ dest/js/app.js
@@ -0,0 +1,73 @@
+"use strict";
+
+/*
+*
+* ============================
+* ============================
+*
+* Include lib:
+*
+* - webFontLoader.js;
+* - preventBehavior.js;
+* - svg4everybody.js;
+*
+* ============================
+* ============================
+* */
+
+/**
+ * @name initPreventBehavior
+ *
+ * @description
+ */
+var initPreventBehavior = function initPreventBehavior() {
+
+  var link = document.querySelectorAll("a");
+
+  link.forEach(function (val, idx) {
+
+    val.addEventListener("click", function (e) {
+      if (val.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
+  });
+};
+
+/**
+ * @description Document DOM ready.
+ */
+$(document).ready(function (ev) {
+  /**
+   *
+   * @type {*|jQuery|HTMLElement}
+   * @private
+   */
+  var _document = $(document),
+      _window = $(window);
+
+  /*
+  * =============================================
+  * CALLBACK :: start
+  * ============================================= */
+
+  /*
+  * CALLBACK :: end
+  * ============================================= */
+
+  /**
+   * @description Init all method
+   */
+  var initJquery = function initJquery() {
+    // default
+    initPreventBehavior();
+    // ==========================================
+
+    // lib
+    // ==========================================
+
+    // callback
+    // ==========================================
+  };
+  initJquery();
+});
---
+++ src/js/_document/indexJquery.js
@@ -0,0 +1,43 @@
+
+
+/**
+ * @description Document DOM ready.
+ */
+$(document).ready((ev) => {
+  /**
+   *
+   * @type {*|jQuery|HTMLElement}
+   * @private
+   */
+  const _document = $(document),
+    _window = $(window);
+
+
+	/*
+	* =============================================
+	* CALLBACK :: start
+	* ============================================= */
+
+	/*
+	* CALLBACK :: end
+	* ============================================= */
+
+
+
+  /**
+   * @description Init all method
+   */
+  const initJquery = () => {
+    // default
+    initPreventBehavior();
+		// ==========================================
+
+    // lib
+		// ==========================================
+
+    // callback
+		// ==========================================
+  };
+  initJquery();
+});
+
